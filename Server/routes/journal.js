70a0948e15ce194e0e76add4da9d6122100e22b0
Lm09 <123.moharana@gmail.com>
2024-02-01 23:53:07 +0530

    Added GET DELETE and UPDATE

--- Server/routes/journal.js
+++ Server/routes/journal.js
@@ -30,4 +30,68 @@ router.post("/journals", authenticateJWT, async (req, res) => {
   }
 });
 
+router.get("/journals", authenticateJWT, async (req, res) => {
+  const userJournals = await Journal.find(
+    { "user._id": req.user.userID },
+    { user: 0, _id: 0, "journal._id": 0, __v: 0 }
+  );
+  console.log(req.user.userID);
+  res.json({
+    journals: userJournals,
+  });
+});
+
+router.get("/journals/:id", authenticateJWT, async (req, res) => {
+  const journal = await Journal.findOne(
+    { _id: req.params.id },
+    { user: 0, _id: 0, "journal._id": 0, __v: 0 }
+  );
+  res.json({
+    journal,
+  });
+});
+
+router.put("/journals/:journalId", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const updatedJournal = await Journal.findByIdAndUpdate(
+      { _id: req.params.journalId },
+      {
+        $set: {
+          journal: {
+            title,
+            content,
+          },
+        },
+      }
+    );
+    res.json({
+      updatedJournal,
+    });
+  } catch (error) {
+    return res.status(400).send("Unable to update the journal");
+  }
+});
+
+router.delete("/journals/:journalId", async (req, res) => {
+  try {
+    const deletedJournal = await Journal.findByIdAndDelete({
+      _id: req.params.journalId,
+    });
+    res.json({
+      deletedJournal,
+    });
+  } catch (error) {
+    return res.status(400).send("Unable to delete the journal");
+  }
+});
+
+// router.use((err, req, res, next) => {
+//   console.log(err);
+
+//   res.status(400).json({
+//     status: "error",
+//   });
+// });
+
 module.exports = router;
